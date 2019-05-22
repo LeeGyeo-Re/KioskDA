7ca73be6c75c1cbfccecd32bc1c75f54ca075f42
LeeGyeo-Re <lgr6952@gmail.com>
2019-05-22 17:56:03 +0900

    Update app.py
    추가추가

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from pymongo import MongoClient
-import threading
 from apscheduler.schedulers.background import BackgroundScheduler
 
 connection = MongoClient("mongodb://master:master1@ds147746.mlab.com:47746/app_7")    #mongodb 주소 입력
 db = connection['app_7']             #mongodb database 입력
 userCollection = db['users']             #collection입력
 recordCollection = db['records']
-interestColeection =  db['interest']
-
+interestCollection = db['interest']
+print("어디가 문제야")
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -21,57 +19,82 @@ def getUser(uuid):
     """
     DB에 있는 User collection중 uuid와 맞는 데이터 가져옴
     """
+    print("어디가 문제야1")
     user = userCollection.find({
         "uuid": uuid
     })
+    print("어디가 문제야1")
     return user
 
 def getUserId():
     """
         DB에 있는 User collection 가져옴
     """
+    print("어디가 문제야2")
     return userCollection.find()
 
-def getRecord(uuid):
+def getRecordTid(uuid):
     """
         DB에 있는 record Usercollection 가져옴(최근 50개)
     """
+    print("어디가 문제야3")
     records = recordCollection.find({
         "uuid": uuid
     })
-    return records[:50]
+    records = list(records)
+    if records:
+        records = records[0]
+    else:
+        records = []
+    print("어디가 문제야3")
+    return records
 
+def countlist(list):
+    print("어디가 문제야4")
+    dic = {}
+    if list:
+        return dic
+    for i in list:
+        if not dic[i]:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    return dic
 def mergeUser(uuid):
     """
     User ID와 record 목록을 가져와서 필요한 데이터 셋으로 합침 
     """
-    result = {}
+    print("어디가 문제야5")
     user = getUser(uuid)
     user = list(user)
     user = user[0]
-    records = getRecord(uuid)
-    records = list(records)
-    if records:
-        records = records[0]
+    records = getRecordTid(uuid)
+    resultlist = []
+    result = {}
+    dicts = {}
 
-    result['uuid'] = uuid
-    if "age" in user:
-        result['age'] = user['age']
-    if "gender" in user:
-        result['gender'] = user['gender']
     if len(records) is 0:
-        result['likes'] = user['likes']
-        result['count'] = 1
+        dicts = countlist(user['likes'])
     else:
-        result['tid'] = list(records)
-        result['count'] = records.count()
-    return result
+        dicts = countlist(records['tid'])
+    for key in dicts.keys():
+        result['uuid'] = uuid
+        if "age" in user:
+            result['age'] = user['age']
+        if "gender" in user:
+            result['gender'] = user['gender']
+        result['tid'] = key
+        result['count'] = dicts[key]
+        resultlist.append(result)
+        print("어디가 문제야5")
+    return resultlist
 
 def startUser(uuid):
     """
     유저가 처음 시작유저인지 판단 
     """
-    if getRecord(uuid):
+    print("어디가 문제야6")
+    if getRecordTid(uuid):
         return False
     else:
         return True
@@ -80,7 +103,8 @@ def getNotInterested(uuid):
     """
     notInterested 가져옴 
     """
-    interest = interestColeection.find({
+    print("어디가 문제야7")
+    interest = interestCollection.find({
         'uuid': uuid
     })
     return interest
@@ -97,29 +121,31 @@ class UserData(object):
         """
         데이터 값 받아서 리스트로 만들어줌
         """
+        print("어디가 문제야8")
         self.resultArr = []
         for ii in getUserId():
             id = ii['uuid']
-            temp = []
             user = mergeUser(id)
-            temp.append(user['uuid'])
-            if startUser(id):
-                temp.append(user['tid'])
-            else:
-                temp.append(user['likes'])
-            temp.append(user['count'])
-            self.resultArr.append(temp)
+            for u in user:
+                temp = []
+                temp.append(u['uuid'])
+                temp.append(u['tid'])
+                temp.append(u['count'])
+                for t in temp:
+                    print(t)
+                self.resultArr.append(temp)
+            print("한줄")
 
 UD = UserData()
 
-
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=UD.makeUserList, trigger="interval", seconds=5)  # seconds로 분기 시간을 조정할 수 있음
+scheduler.start()
 @app.route('/recommand')
 def recommand():
     return "분석된 데이터 전송"
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=UD.makeUserList, trigger="interval", seconds=3600)   #seconds로 분기 시간을 조정할 수 있음
-scheduler.start()
+
 if __name__ == '__main__':
     print("Starting webapp...")
     app.run()
