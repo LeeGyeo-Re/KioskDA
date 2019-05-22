6a581d4ff8d93d3732d0b0f304615229698840ea
LeeGyeo-Re <lgr6952@gmail.com>
2019-05-22 11:19:44 +0900

    Update app.py
    주석 추가욤

--- app.py
+++ app.py
@@ -18,21 +18,33 @@ def hello_world():
 
 
 def getUser(uuid):
+    """
+    DB에 있는 User collection중 uuid와 맞는 데이터 가져옴
+    """
     user = userCollection.find({
         "uuid": uuid
     })
     return user
 
 def getUserId():
+    """
+        DB에 있는 User collection 가져옴
+    """
     return userCollection.find()
 
 def getRecord(uuid):
+    """
+        DB에 있는 record Usercollection 가져옴(최근 50개)
+    """
     records = recordCollection.find({
         "uuid": uuid
     })
     return records[:50]
 
 def mergeUser(uuid):
+    """
+    User ID와 record 목록을 가져와서 필요한 데이터 셋으로 합침 
+    """
     result = {}
     user = getUser(uuid)
     user = list(user)
@@ -56,19 +68,28 @@ def mergeUser(uuid):
     return result
 
 def startUser(uuid):
+    """
+    유저가 처음 시작유저인지 판단 
+    """
     if getRecord(uuid):
         return False
     else:
         return True
 
 def getNotInterested(uuid):
+    """
+    notInterested 가져옴 
+    """
     interest = interestColeection.find({
         'uuid': uuid
     })
     return interest
 
 class UserData(object):
-
+    """
+    해당 클래스는 받아온 유저 데이터를 리스트에 저장하기 위한 목적으로 resultArr에 저장
+    makeUserList()는 resultArr을 갱신하는 함수(정기적으로)
+    """
     def __init__(self):
         self.resultArr = []
 
@@ -97,7 +118,7 @@ def recommand():
     return "분석된 데이터 전송"
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=UD.makeUserList, trigger="interval", seconds=3600)
+scheduler.add_job(func=UD.makeUserList, trigger="interval", seconds=3600)   #seconds로 분기 시간을 조정할 수 있음
 scheduler.start()
 if __name__ == '__main__':
     print("Starting webapp...")
